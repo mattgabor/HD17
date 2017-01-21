823d1e54eaf334e5b1560563362f293d4b7893b7
Matt Gabor <mattgabor@gmail.com>
2017-01-21 15:48:22 -0800

    added public code

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
 app.post('/analyze', function(req, res) {
   var text = req.body.toAnalyze;
   console.log(text);
   res.send('done');
 });
 
-app.use(express.static('.'));
+app.use(express.static('./public'));
 http.createServer(app).listen(8080);
 
 console.log('Server started on localhost:8080');
